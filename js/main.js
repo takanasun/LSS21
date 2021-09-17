7a989f778a1e4e3738574f485c994ea5dec9b04e
sc_kana_takahashi <kana.takahashi@s-cubism.jp>
2021-09-18 03:11:37 +0900

    複数解説対応

--- js/main.js
+++ js/main.js
@@ -18,13 +18,27 @@ $(function(){
             var textLength = data.split(",");
             $("#cate").text(textLength[0]);
             
+            var colArr = ["comName","comID"];
+
             for(let i=0;i < obj.length;i++){
                 var clname = "#"+ obj[i];
                 if(obj[i] == "comName" && textLength[colNum[i]] == "") {
                     $("#commentator").css("display","none");
+                    $("#commentator2").css("display","none");
+                    $(".freeSpace").css("height","300px");
+                }else if(obj[i] == "comName" && textLength[colNum[i]].indexOf("/") < 1){
+                    $(".freeSpace").css("height","210px");
+                    $("#commentator2").css("display","none");
                 }
-
-                if(obj[i].match(/Icon/) && textLength[colNum[i]] != ""){
+                if(obj[i] == "comIcon" &&  textLength[colNum[i]].indexOf("/") >= 1){
+                    var listIcon = textLength[colNum[i]].split("/");
+                    for (let index = 0; index < listIcon.length; index++) {
+                        const element = listIcon[index];
+                        $(clname + (index + 1)).addClass(element);
+                    }
+                }else if(obj[i] == "comIcon" && textLength[colNum[i]] != "" ){
+                        $(clname +  1).addClass( textLength[colNum[i]]);
+                }else if(obj[i].match(/Icon/) && textLength[colNum[i]] != ""){
                     $(clname).addClass(textLength[colNum[i]]);
                 }else if(obj[i] == "title" && textLength[colNum[i]] != ""){
                     var titleLen = textLength[colNum[i]].replace(/\r?\n/g , "");                    
@@ -56,6 +70,13 @@ $(function(){
                     $(clname).text("Category:" + textLength[colNum[i]]);
                 }else if(obj[i] == "nextCommentator" && textLength[colNum[i]] == ""){
                     $(clname).remove();
+                }else if(colArr.includes(obj[i]) && textLength[colNum[i]] != "" ){
+                    var listCom = textLength[colNum[i]].split("/");
+                    for (let index = 0; index < listCom.length; index++) {
+                        const element = listCom[index];
+                        $(clname + (index + 1)).text(listCom[index]); 
+                    }
+                    
                 }else{
                     $(clname).text(textLength[colNum[i]]);    
                 }
